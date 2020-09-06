f6a98e2143f983a03da8dc232ce1aaf5b60ab6dd
tquangdo <trangia61@gmail.com>
2020-09-06 16:57:12 +0900

    20200906 16:57

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,18 +1,3 @@
-/* API_KEY
-1) https://developers.google.com/custom-search/v1/using_rest
-2) click "identify your application" of [API key]
-3) click "Get a Key"
-4) [Enable Custom Search API] select project="google-clone"
-*/
-
-/* CONTEXT_KEY
-1) https://cse.google.com/cse/create/new
-2) Sites to search=www.google.com > Create
-3) click "Get a Key"
-4) Edit search engine > Setup > Basics > Search the entire web="ON"
-5) Copy to clipboard "Search engine ID"
-*/
-
 import { useEffect, useState } from "react";
 import * as keys from "./Keys";
 
